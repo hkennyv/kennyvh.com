bac695666b5d587f43dd185838415fc16c75ea92
khuynh <hkennyv@gmail.com>
2020-03-22 15:13:56 -0700

    add bio to index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,14 +7,51 @@ import PostListing from "../components/PostListing";
 import config from "../../data/SiteConfig";
 
 class IndexPage extends Component {
+  renderBio = () => {
+    return (
+      <div className="bio">
+        <h1>{`Hi there! I'm Kenny. 👋`}</h1>
+        <p>
+          I'm a <strong>DevOps Engineer</strong> at{"  "}
+          <a href="http://www.rejouleenergy.com/">
+            <strong>ReJoule, Inc.</strong>
+          </a>
+          We are an early-stage startup with an amazing team whose goal is to
+          maximize the value of every battery through advanced battery
+          diagnostics.
+        </p>
+        <p>
+          I'm interested in solving high societal/environmental impact problems
+          through technology and engineering. I love learning and experimenting
+          with technology across the entire stack (from making schematics and
+          laying out PCBs to writing drivers to software interfaces and APIs).
+        </p>
+        <p>
+          I'm originally from Tracy, CA and now live in
+          <strong>Garden Grove, CA</strong>. Some of my interests outside of
+          work include:
+        </p>
+        <ul className="interests">
+          <li>bicycles ️🚲</li>
+          <li>cats 🐱</li>
+          <li>computers 💻</li>
+          <li>motorcycles 🏍</li>
+          <li>powerlifting 🏋️‍♀️</li>
+          <li>traveling 🧳</li>
+        </ul>
+      </div>
+    );
+  };
+
   render() {
     const { data } = this.props;
     const postEdges = data.latest.edges;
 
     return (
       <Layout>
-        <div className="index-container">
-          <Helmet title={`Index | ${config.siteTitle}`} />
+        <Helmet title={`Kenny Huynh`} />
+        <div className="container">
+          {this.renderBio()}
           <h3>LATEST POSTS</h3>
           <PostListing postEdges={postEdges} />
         </div>
