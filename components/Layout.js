7badad932526aca5aa55faa81c97876ae20e7218
Kenny Huynh <hkennyv@gmail.com>
2020-08-06 23:29:54 -0700

    add netlify cms scripts

--- components/Layout.js
+++ components/Layout.js
@@ -6,6 +6,7 @@ export default function Layout({ children, pageTitle, ...props }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         <title>{pageTitle}</title>
       </Head>
       <Header />
