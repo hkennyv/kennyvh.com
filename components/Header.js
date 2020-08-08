49b73dc22a9e458d6275862c20d172bcbbb0ca4a
Kenny Huynh <hkennyv@gmail.com>
2020-08-08 16:08:09 -0700

    make header responsive

--- components/Header.js
+++ components/Header.js
@@ -2,29 +2,25 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <>
-      <header className="header">
-        <nav className="nav">
-          <ul className="nav__list">
-            {/* TODO: refactor this into Links based on pages in `pages` directory */}
-            <li>
-              <Link href="/">
-                <a>home</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/about">
-                <a>about</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/blog">
-                <a>blog</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </>
+    <nav className="nav">
+      <ul className="nav__list">
+        {/* TODO: refactor this into Links based on pages in `pages` directory */}
+        <li>
+          <Link href="/">
+            <a>home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>about</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/blog">
+            <a>blog</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
