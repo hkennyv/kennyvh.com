5aa64023bd5cc35ee853ccd4f6b4bdf6c5806c13
Kenny Huynh <hkennyv@gmail.com>
2020-09-10 20:01:11 -0700

    chore: add opengraph meta tags to layout

--- components/Layout.js
+++ components/Layout.js
@@ -9,6 +9,14 @@ export default function Layout({ children, pageTitle, ...props }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        <meta property="og:title" content="Kenny Huynh" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.kennyvh.com/" />
+        <meta
+          property="og:image"
+          content="https://pbs.twimg.com/profile_banners/603264551/1431934867/1500x500"
+        />
+
         <title>{pageTitle}</title>
       </Head>
       <div className="main">
